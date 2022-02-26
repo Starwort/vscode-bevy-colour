631950c55f3bc5b229d1cceab26b8a3304e6d817
MarkosTh09 <markos.theocharis2009@gmail.com>
2022-02-26 21:35:23 +0200

    removed remote add from action

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 echo "Pushing changes to $1"
-if git ls-remote --exit-code azure --quiet; then
-    git remote add azure "$1"
-fi
 
-git pull azure main
-
-git push -f azure main
+git push -f $1 main
